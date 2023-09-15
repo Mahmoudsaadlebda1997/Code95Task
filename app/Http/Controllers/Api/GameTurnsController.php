06439487dfc617aa0b5d0f4a3b5a87c2515e25ed
Mahmoudsaadlebda1997 <mahmodsaadlebda@gmail.com>
2023-09-15 08:22:53 +0300

    addign Fix

--- app/Http/Controllers/Api/GameTurnsController.php
+++ app/Http/Controllers/Api/GameTurnsController.php
@@ -8,6 +8,40 @@ use SplQueue;
 
 class GameTurnsController extends Controller
 {
+
+    // Array Soullion
+    public function getGameTurns(Request $request)
+    {
+        $players = $request->playersNumber;
+        $turns = $request->turns;
+        $startingPlayer = $request->startingPlayer;
+
+        $playersArray = range('A', 'Z'); // Array of players from A to Z
+
+        // Find the index of the startingPlayer in the playersArray
+        $startingIndex = array_search($startingPlayer, $playersArray);
+
+        $turnsArray = [];
+        for ($i = 0; $i < $turns; $i++) {
+            $turn = [];
+            $playerIndex = $startingIndex;
+
+            for ($j = 0; $j < $players; $j++) {
+                $player = $playersArray[$playerIndex];
+                $turn[] = $player;
+                $playerIndex = ($playerIndex + 1) % $players;
+            }
+
+            // Rotate the playersArray to the left
+            $firstPlayer = array_shift($playersArray);
+            array_push($playersArray, $firstPlayer);
+
+            $turnsArray[] = $turn;
+        }
+
+        return response()->json($turnsArray);
+    }
+//    //Queye sollution
 //    public function getGameTurns(Request $request)
 //    {
 //        $players = $request->playersNumber;
@@ -15,65 +49,29 @@ class GameTurnsController extends Controller
 //        $startingPlayer = $request->startingPlayer;
 //
 //        $turnsArray = [];
+//        $playersQueue = new SplQueue();
+//
+//        // Initialize the queue with players
+//        for ($i = 0; $i < $players; $i++) {
+//            $playersQueue->enqueue(chr(ord($startingPlayer) + $i));
+//        }
+//
 //        for ($i = 0; $i < $turns; $i++) {
-//            $turnsArray[] = $this->getTurn($players, $startingPlayer);
-//            $startingPlayer = $this->getNextPlayer($startingPlayer);
+//            $turn = [];
+//            $queueSize = $playersQueue->count();
+//
+//            for ($j = 0; $j < $queueSize; $j++) {
+//                $player = $playersQueue->dequeue();
+//                $turn[] = $player;
+//                $playersQueue->enqueue($player);
+//            }
+//
+//            $startingPlayer = $playersQueue->dequeue();
+//            $playersQueue->enqueue($startingPlayer);
+//
+//            $turnsArray[] = $turn;
 //        }
 //
 //        return response()->json($turnsArray);
-//    }
-    public function getGameTurns(Request $request)
-    {
-        $players = $request->playersNumber;
-        $turns = $request->turns;
-        $startingPlayer = $request->startingPlayer;
-
-        $turnsArray = [];
-        $playersQueue = new SplQueue();
-
-        // Initialize the queue with players
-        for ($i = 0; $i < $players; $i++) {
-            $playersQueue->enqueue(chr(ord($startingPlayer) + $i));
-        }
-
-        for ($i = 0; $i < $turns; $i++) {
-            $turn = [];
-            $queueSize = $playersQueue->count();
-
-            for ($j = 0; $j < $queueSize; $j++) {
-                $player = $playersQueue->dequeue();
-                $turn[] = $player;
-                $playersQueue->enqueue($player);
-            }
-
-            $startingPlayer = $playersQueue->dequeue();
-            $playersQueue->enqueue($startingPlayer);
-
-            $turnsArray[] = $turn;
-        }
-
-        return response()->json($turnsArray);
-    }
-// To Get The Turns Number
-//    private function getTurn($players, $startingPlayer)
-//    {
-//        $turn = [];
-//        $alphabet = range('A', 'Z');
-//        $startingPlayerIndex = array_search($startingPlayer, $alphabet);
-//
-//        for ($i = 0; $i < $players; $i++) {
-//            $playerIndex = ($startingPlayerIndex + $i) % $players;
-//            $turn[] = $alphabet[$playerIndex];
-//        }
-//
-//        return $turn;
-//    }
-//
-//    private function getNextPlayer($player)
-//    {
-//        $alphabet = range('A', 'Z');
-//        $playerIndex = array_search($player, $alphabet);
-//        $nextPlayerIndex = ($playerIndex + 1) % count($alphabet);
-//        return $alphabet[$nextPlayerIndex];
 //    }
 }
